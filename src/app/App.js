74ecd08cb815561e8384ab15a50fc4cd93450032
Avaz <elonarm21@gmail.com>
2023-11-29 00:18:12 +0500

    ready to start

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className={"app"}>
+            <div className={"container"}>
+                <h1 className={"text-center my-4"}>Hello world</h1>
+            </div>
+        </div>
+    );
+};
+
+export default App;
