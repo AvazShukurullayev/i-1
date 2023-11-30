0167aede036ab8a26e75f0bf5915077202f4920e
Avaz <elonarm21@gmail.com>
2023-11-30 16:04:46 +0500

    About createStore

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.css"
-import App from "./app/App";
+import {createStore} from "redux";
+
+const initialState = 0
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "inc":
+            return state + 1;
+        case "dec":
+            return state - 1;
+        case "res":
+            return 0;
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer)
+store.subscribe(()=> {
+    document.getElementById("count").textContent = store.getState()
+})
+document.getElementById("inc").addEventListener("click", () => {
+    store.dispatch({type: "inc"})
+})
+document.getElementById("res").addEventListener("click", () => {
+    store.dispatch({type: "res"})
+})
+document.getElementById("dec").addEventListener("click", () => {
+    store.dispatch({type: "dec"})
+})
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <></>
+    </React.StrictMode>
 );
